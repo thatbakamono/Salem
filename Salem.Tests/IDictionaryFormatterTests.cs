1e9fdd6c44dbd244fc507c300b1e00fb20917cad
KernelError <villainkernelerror@gmail.com>
2019-08-09 14:53:58 +0200

    Add tests for IDictionaryFormatter and IListFormatter

---
+++ Salem.Tests/IDictionaryFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Pastel;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Salem.Tests
+{
+    public class IDictionaryFormatterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var logger = new Logger("");
+            var output = new SyntheticOutput();
+            var dictionary = new Dictionary<string, string> { { "1", "first" }, { "2", "second" } };
+
+            logger.Outputs.Clear();
+            logger.Outputs.Add(output);
+
+            logger.Log("info", dictionary);
+
+            Assert.Equal($"{"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "1 = first".Pastel(Color.LightGray) }", output.Lines[0]);
+            Assert.Equal($"             { "  2 = second".Pastel(Color.Gray) }", output.Lines[1]);
+        }
+    }
+}
