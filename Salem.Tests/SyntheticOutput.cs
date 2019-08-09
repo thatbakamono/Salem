7b192fba8a58460e7c60bfb77cc31a1ad2a8336a
KernelError <villainkernelerror@gmail.com>
2019-08-09 14:52:31 +0200

    Add class which makes testing easier

---
+++ Salem.Tests/SyntheticOutput.cs
@@ -0,0 +1,18 @@
+﻿using Salem.Outputs;
+using System.Collections.Generic;
+
+namespace Salem.Tests
+{
+    public class SyntheticOutput : IOutput
+    {
+        public IReadOnlyList<string> Lines => lines;
+
+        private List<string> lines = new List<string>();
+
+        public void WriteLine(string input) => 
+            lines.Add(input);
+
+        public void Clear() => 
+            lines.Clear();
+    }
+}
