6edfe08e2e2cc5a0af05598f8965429ae53557ed
KernelError <villainkernelerror@gmail.com>
2019-08-09 16:13:38 +0200

    Add new tests and update previous ones

--- Salem.Tests/IDictionaryFormatterTests.cs
+++ Salem.Tests/IDictionaryFormatterTests.cs
@@ -19,8 +19,24 @@ namespace Salem.Tests
 
             logger.Log("info", dictionary);
 
-            Assert.Equal($"{"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "1 = first".Pastel(Color.LightGray) }", output.Lines[0]);
-            Assert.Equal($"             { "  2 = second".Pastel(Color.Gray) }", output.Lines[1]);
+            Assert.Equal($"{"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "1 = first".Pastel(logger.Colors[1]) }", output.Lines[0]);
+            Assert.Equal($"             { "  2 = second".Pastel(logger.Colors[1]) }", output.Lines[1]);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var logger = new Logger("scope");
+            var output = new SyntheticOutput();
+            var dictionary = new Dictionary<string, string> { { "1", "first" }, { "2", "second" } };
+
+            logger.Outputs.Clear();
+            logger.Outputs.Add(output);
+
+            logger.Log("info", dictionary);
+
+            Assert.Equal($"{ "[scope]".Pastel(logger.Colors[0]) } {"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "1 = first".Pastel(logger.Colors[1]) }", output.Lines[0]);
+            Assert.Equal($"{ "[scope]".Pastel(logger.Colors[0]) }              { "  2 = second".Pastel(logger.Colors[1]) }", output.Lines[1]);
         }
     }
 }
--- Salem.Tests/IListFormatterTests.cs
+++ Salem.Tests/IListFormatterTests.cs
@@ -19,8 +19,24 @@ namespace Salem.Tests
 
             logger.Log("info", list);
 
-            Assert.Equal($"{"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "first".Pastel(Color.LightGray) }", output.Lines[0]);
-            Assert.Equal($"             { "  second".Pastel(Color.Gray) }", output.Lines[1]);
+            Assert.Equal($"{"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "first".Pastel(logger.Colors[1]) }", output.Lines[0]);
+            Assert.Equal($"             { "  second".Pastel(logger.Colors[1]) }", output.Lines[1]);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var logger = new Logger("scope");
+            var output = new SyntheticOutput();
+            var list = new List<string> { "first", "second" };
+
+            logger.Outputs.Clear();
+            logger.Outputs.Add(output);
+
+            logger.Log("info", list);
+
+            Assert.Equal($"{ "[scope]".Pastel(logger.Colors[0]) } {"[i]".Pastel(Color.Cyan)} { Ansi.Underline("Info").Pastel(Color.Cyan) + "      " } { "first".Pastel(logger.Colors[1]) }", output.Lines[0]);
+            Assert.Equal($"{ "[scope]".Pastel(logger.Colors[0]) }              { "  second".Pastel(logger.Colors[1]) }", output.Lines[1]);
         }
     }
 }
